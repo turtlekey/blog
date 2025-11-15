013764ffe902c81bdff4f706b1e7048bce850d29
turtlekey <turtlekey011@gmail.com>
2025-11-15 21:45:00 +0800

    optimize stock

---
+++ docs/js/main.js
@@ -0,0 +1,10 @@
+(() => {
+  // <stdin>
+  function goHome() {
+    const eleTitle = document.querySelector("#blog-title");
+    eleTitle.addEventListener("pointerdown", () => window.location.href = "/");
+  }
+  document.addEventListener("DOMContentLoaded", function() {
+    goHome();
+  });
+})();
