2790cb62a6352296736cb5bad63b5a8f0893bed4
turtlekey <turtlekey011@gmail.com>
2025-11-15 22:20:01 +0800

    set tooltip.alwaysShowContent to false

---
+++ docs/js/main.72f27bb804669d15c7d4ac6b232e9f90a56dcae637aed8dccd8aff55723f0c89.js
@@ -0,0 +1,1 @@
+(()=>{function e(){document.querySelector("#blog-title").addEventListener("pointerdown",()=>window.location.href="/blog/")}document.addEventListener("DOMContentLoaded",function(){e()});})();
